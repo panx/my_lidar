d09bd0891a68285f20751b152fc6294948cda963
panx <panx@lidar.localdomain>
2012-12-16 01:10:29 -0500

    add sub_vel

---
+++ my_lidar_code_final/sub_vel/build/CMakeFiles/listener_vel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/listener_vel.dir/src/listener_vel.o"
+  "../bin/listener_vel.pdb"
+  "../bin/listener_vel"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/listener_vel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
